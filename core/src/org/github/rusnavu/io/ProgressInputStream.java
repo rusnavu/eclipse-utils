aa34ff764ce370b4f574d815311b315bf6576a86
A.Nikitin <rusnavu@gmail.com>
2015-04-29 15:13:53 +0300

    [*] ProgressInputStream

--- core/src/org/github/rusnavu/io/ProgressInputStream.java
+++ core/src/org/github/rusnavu/io/ProgressInputStream.java
@@ -10,36 +10,47 @@ public class ProgressInputStream extends InputStream {
 
 	private final InputStream stream;
 	private final IProgressMonitor monitor;
-
-	public ProgressInputStream(InputStream stream, IProgressMonitor monitor) {
-		this(stream, monitor, IProgressMonitor.UNKNOWN);
-	}
+	private final int scale;
 
 	public ProgressInputStream(InputStream stream, IProgressMonitor monitor, int ticks) {
+		this(stream, monitor, ticks, IProgressMonitor.UNKNOWN);
+	}
+
+	public ProgressInputStream(InputStream stream, IProgressMonitor monitor, int ticks, long length) {
+		assert length == IProgressMonitor.UNKNOWN || length > 0;
 		this.stream = stream;
-		this.monitor = new SubProgressMonitor(monitor, ticks);
+		int totalWork;
+		if ((length == IProgressMonitor.UNKNOWN) || (length < Integer.MAX_VALUE)) {
+			totalWork = (int) length;
+			scale = 1;
+		} else {
+			scale = (int) (length / Integer.MAX_VALUE);
+			totalWork = (int) (length / scale);
+		}
+		this.monitor = new SubProgressMonitor(monitor, totalWork);
+		this.monitor.beginTask("", ticks);
 	}
 
 	private int worked(int readed) {
-		monitor.worked(readed);
+		monitor.worked(readed/scale);
 		return readed;
 	}
 
 	@Override
 	public int read(byte[] b) throws IOException {
-		return worked(stream.read(b));
+		return worked(stream.read(b)/scale);
 	}
 
 	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
-		return worked(stream.read(b, off, len));
+		return worked(stream.read(b, off, len)/scale);
 	}
 
 	@Override
 	public int read() throws IOException {
 		int read;
 		if ((read = stream.read()) >= 0)
-			monitor.worked(1);
+			monitor.worked(1/scale);
 		return read;
 	}
 
